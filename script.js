d9d5332eef7a9288b776e6938cb4d857f194628d
Andrew <andrew@andrewsmith.org>
2022-10-27 22:48:30 -0400

    Implement adding text from array on click

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 const gridItems = document.querySelectorAll(".grid-item");
 
-gridItems.forEach((item) => {
+gridItems.forEach((item, i) => {
   item.addEventListener("click", function (e) {
     e.target.style.background = "blue";
     // not working quite as desired -- if hover target
     // is <p>, this only turns the background of the p
     // element blue rather than the whole div
+    e.target.innerText = `"${descriptions[i]}"`;
   });
 });
+
+const descriptions = ["test text 1", "test text 2"];
