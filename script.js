d92895088dbc80ce2a793e495ecb458ec693ba33
Andrew <andrew@andrewsmith.org>
2022-10-28 21:42:58 -0400

    Refactor for maintainability/cleanliness

--- script.js
+++ script.js
@@ -1,27 +1,37 @@
-const gridItems = document.querySelectorAll(".grid-item");
+// const gridItems = document.querySelectorAll(".grid-item");
 
-let states = [];
+const data = [
+  { symbol: "foo", description: "bar", state: "symbol" },
+  { symbol: "foo", description: "bar", state: "symbol" },
+  { symbol: "foo", description: "bar", state: "symbol" },
+  { symbol: "foo", description: "bar", state: "symbol" },
+  { symbol: "foo", description: "bar", state: "symbol" },
+  { symbol: "foo", description: "bar", state: "symbol" },
+  { symbol: "foo", description: "bar", state: "symbol" },
+  { symbol: "foo", description: "bar", state: "symbol" },
+  { symbol: "foo", description: "bar", state: "symbol" },
+];
+
+const gridItems = data.map((item) => {
+  const newDiv = document.createElement("div");
+  const newP = document.createElement("p");
+  newDiv.setAttribute("class", "grid-item");
+  newP.textContent = item.symbol;
+  document.querySelector(".grid").appendChild(newDiv);
+  newDiv.appendChild(newP);
+  return newDiv;
+});
 
 gridItems.forEach((item, i) => {
-  states.push("symbol");
   item.addEventListener("click", function (e) {
-    if (states[i] == "symbol") {
-      e.target.style.background = "blue";
-      // not working quite as desired -- if hover target
-      // is <p>, this only turns the background of the p
-      // element blue rather than the whole div
-      let content = document.createElement("p");
-      content.textContent = `${descriptions[i]}`;
-      content.setAttribute("id", "box " + i + " text");
-      gridItems[i].appendChild(content);
-      states[i] = "description";
-    } else if (states[i] == "description") {
-      e.target.style.background = "rgba(16, 51, 11, 0.6)";
-      content = document.getElementById("box " + i + " text");
-      gridItems[i].removeChild(content);
-      states[i] = "symbol";
+    if (data[i].state == "symbol") {
+      item.style.background = "blue";
+      item.firstChild.textContent = `${data[i].description}`;
+      data[i].state = "description";
+    } else if (data[i].state == "description") {
+      item.style.background = "rgba(16, 51, 11, 0.6)";
+      item.firstChild.textContent = `${data[i].symbol}`;
+      data[i].state = "symbol";
     }
   });
 });
-
-const descriptions = ["test text 1", "test text 2"];
